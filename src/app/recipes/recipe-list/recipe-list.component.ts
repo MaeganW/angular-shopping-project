969bb97cf4aba7dc03691e4d08c6a1cba751f84b
maeganW <maeganwomble21@gmail.com>
2017-11-06 16:56:24 -0600

    setting up recipe list component structure

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test', 'this is a test', 'https://en.wikipedia.org/wiki/Meal#/media/File:Floris_Claesz._van_Dyck_001.jpg');
+  ];
 
   constructor() { }
 
