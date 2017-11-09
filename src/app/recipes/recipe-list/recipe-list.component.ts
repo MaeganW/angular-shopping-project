e996a49b0e80c888d2ddffe4b92731a1413c971e
maeganW <maeganwomble21@gmail.com>
2017-11-09 14:13:03 -0600

    passing child info through two parents to another child - from recipe item to recipe list to recipes to recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() selectedRecipeLvl2 = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'Hamburger',
@@ -19,6 +20,10 @@ export class RecipeListComponent implements OnInit {
       'http://drop.ndtv.com/albums/COOKS/corngallery/creolespicedcornthumb_640x480.jpg'
     )
   ];
+
+  passOnInfo(recipe: Recipe) {
+    this.selectedRecipeLvl2.emit(recipe);
+  }
 
   constructor() { }
 
