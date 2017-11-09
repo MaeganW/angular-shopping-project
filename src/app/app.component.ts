14b3fa8033eb68349826fc8db2c730c70a551e5e
maeganW <maeganwomble21@gmail.com>
2017-11-09 11:02:02 -0600

    using @output, EventEmitter, and ngIf to navigate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  loadedSection = 'recipes';
+
+  onSelectingSection(section: string) {
+    this.loadedSection = section;
+  }
 }
